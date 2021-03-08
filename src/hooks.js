5be7980c00e3631e8b60cc5b9f6cceb2218ca9b6
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-08 15:52:46 +0530

    1. Prototype a few form hooks - will need to reassess later

---
+++ src/hooks.js
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import MuiPhoneInput from 'material-ui-phone-number';
+import { DATE_T, EMAIL_T, NUMBER_T, STRING_T } from './constants';
+
+const useFormState = () => {
+  const [currentForm, setCurrentForm] = useState(
+    [
+      {
+        fieldType: STRING_T,
+        fieldName: 'firstName',
+        fieldLabel: 'First Name',
+        value: '',
+        properties: {},
+      },
+      {
+        fieldType: STRING_T,
+        fieldName: 'lastName',
+        fieldLabel: 'Last Name',
+        value: '',
+        properties: {},
+      },
+      {
+        fieldType: EMAIL_T,
+        fieldName: 'email',
+        fieldLabel: 'Email',
+        value: '',
+        properties: {},
+      },
+      {
+        fieldType: STRING_T,
+        fieldName: 'address',
+        fieldLabel: 'Address',
+        value: '',
+        properties: {
+          multiline: true,
+          rowsMax: 5,
+          fullWidth: true,
+        },
+      },
+      {
+        fieldType: NUMBER_T,
+        fieldName: 'phone',
+        fieldLabel: 'Phone',
+        SpecificElement: MuiPhoneInput,
+        value: null,
+        properties: {},
+      },
+      {
+        fieldType: DATE_T,
+        fieldName: 'dob',
+        fieldLabel: 'Date of Birth',
+        value: '',
+        properties: {},
+      },
+    ].map((current) => ({ ...current, id: nanoid() }))
+  );
+
+  const setField = (idToLookFor, val) => {
+    const idxOfField = currentForm.indexOf(({ id }) => id === idToLookFor);
+    const newForm = currentForm
+      .slice()
+      .splice(idxOfField, 1, { ...currentForm[idxOfField], value: val });
+    setCurrentForm(newForm);
+  };
+
+  return [currentForm, setField];
+};
+
+/**
+ * @param {object} specificationList - Must have items as objects with fields fieldName and fieldType, as well as a value field that could be an empty string
+ */
+const useAdditionalFormState = (specificationList) => {
+  const [currentForm, setCurrentForm] = useState(
+    specificationList.map((current) => ({ ...current, id: nanoid() }))
+  );
+
+  const setField = (idToLookFor, val) => {
+    const idxOfField = currentForm.indexOf(({ id }) => id === idToLookFor);
+    const newForm = currentForm
+      .slice()
+      .splice(idxOfField, 1, { ...currentForm[idxOfField], value: val });
+    setCurrentForm(newForm);
+  };
+
+  return [currentForm, setField];
+};
+
+export { useFormState, useAdditionalFormState };

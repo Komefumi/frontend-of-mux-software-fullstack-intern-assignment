9638a39279a77a8ed9a31fdb4cc2b15e01f996d6
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-09 21:04:15 +0530

    Remove unnecessary hooks

--- src/hooks.js
+++ src/hooks.js
@@ -14,91 +14,6 @@ import {
   UNSET_DIALOG,
 } from './constants';
 
-const useFormState = () => {
-  const [currentForm, setCurrentForm] = useState(
-    [
-      {
-        fieldType: STRING_T,
-        fieldName: 'firstName',
-        fieldLabel: 'First Name',
-        value: '',
-        properties: {},
-      },
-      {
-        fieldType: STRING_T,
-        fieldName: 'lastName',
-        fieldLabel: 'Last Name',
-        value: '',
-        properties: {},
-      },
-      {
-        fieldType: EMAIL_T,
-        fieldName: 'email',
-        fieldLabel: 'Email',
-        value: '',
-        properties: {},
-      },
-      {
-        fieldType: STRING_T,
-        fieldName: 'address',
-        fieldLabel: 'Address',
-        value: '',
-        properties: {
-          multiline: true,
-          rowsMax: 5,
-          fullWidth: true,
-        },
-      },
-      {
-        fieldType: NUMBER_T,
-        fieldName: 'phone',
-        fieldLabel: 'Phone',
-        SpecificElement: MuiPhoneInput,
-        value: null,
-        properties: {},
-      },
-      {
-        fieldType: DATE_T,
-        fieldName: 'dob',
-        fieldLabel: 'Date of Birth',
-        value: '',
-        properties: {},
-      },
-    ].map((current) => ({ ...current, id: nanoid() }))
-  );
-
-  const setField = (idToLookFor, val) => {
-    const idxOfField = currentForm.indexOf(({ id }) => id === idToLookFor);
-    const newForm = currentForm
-      .slice()
-      .splice(idxOfField, 1, { ...currentForm[idxOfField], value: val });
-    setCurrentForm(newForm);
-  };
-
-  return [currentForm, setField];
-};
-
-/**
- * @param {object} specificationList - Must have items as objects with fields fieldName and fieldType, as well as a value field that could be an empty string
- */
-const useAdditionalFormState = (specificationList) => {
-  const [currentForm, setCurrentForm] = useState(
-    specificationList.map((current) => {
-      return { ...current, id: nanoid() };
-    })
-  );
-
-  const setField = (idToLookFor, val) => {
-    const idxOfField = currentForm.indexOf(({ id }) => id === idToLookFor);
-    const newForm = currentForm
-      .slice()
-      .splice(idxOfField, 1, { ...currentForm[idxOfField], value: val });
-    setCurrentForm(newForm);
-  };
-
-  return [currentForm, setField, setCurrentForm];
-};
-
 const useSuccessFlash = () => {
   const dispatch = useDispatch();
   const setSuccessFlash = (flashMessage) => {
@@ -142,11 +57,4 @@ const useDialogClose = () => {
   return closeDialog;
 };
 
-export {
-  useFormState,
-  useAdditionalFormState,
-  useSuccessFlash,
-  useErrorFlash,
-  useDialogSetter,
-  useDialogClose,
-};
+export { useSuccessFlash, useErrorFlash, useDialogSetter, useDialogClose };
